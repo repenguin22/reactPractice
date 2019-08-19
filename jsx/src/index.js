c971687eef7aade16bff3c8e22dd6463c8141f46
repenguin <devSomaPro@gmail.com>
2019-08-20 08:15:17 +0900

    jsxに対応
    styleをjsx記法に修正
    classはclassNameを使う
    {}でjsの変数を使うことが可能ただし制限あり

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 // create react component
 const App = () => {
-    return <div>Hi there!</div>
+
+    return (
+        <div>
+            <label className="label" for="name">Enter Name:</label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
+        </div>
+    )
 }
 
 // take the react component and show it on the screen
