a0956c78c6a8fb39c8a5d1d9e0de0dd800dcbfa3
repenguin <devSomaPro@gmail.com>
2019-08-21 06:28:30 +0900

    for => htmlFor

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -9,7 +9,7 @@ const App = () => {
 
     return (
         <div>
-            <label className="label" for="name">Enter Name:</label>
+            <label className="label" htmlFor="name">Enter Name:</label>
             <input id="name" type="text" />
             <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
         </div>
