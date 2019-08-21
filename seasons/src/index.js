df9112705441921995f55a8c8113e3eb276de846
repenguin <devSomaPro@gmail.com>
2019-08-22 08:14:14 +0900

    init

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    return <div>Hi There</div>
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
 };
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
+
+
+}
 
 ReactDOM.render(
     <App />,
