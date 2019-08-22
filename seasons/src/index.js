6edc4e4dc2a09ad91f4ee580ff1c56451b62d427
repenguin <devSomaPro@gmail.com>
2019-08-23 08:20:42 +0900

    lifecycle
    ライフサイクルを適応、コンストラクターを削除

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null };
+    state = { lat: null, errMsg: '' };
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => console.log(err)
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errMsg: err.message })
         );
     }
 
     render() {
-        return <div>Latitude: {this.state.lat}</div>;
+        return (
+            <div>
+                <SeasonDisplay lat={this.state.lat} />
+                <br />
+                Error: {this.state.errMsg}
+            </div>
+        );
     }
 
 
