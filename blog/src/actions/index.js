f57e25756484e452739ef9a0278762153bac7738
repenguin <devSomaPro@gmail.com>
2019-09-04 08:17:57 +0900

    refactor まとめて一意のユーザを取得する

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,15 +1,20 @@
 import lodash from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    //const userIds = lodash.uniq(lodash.map(getState().posts, 'userId'));
+    //userIds.forEach(id => dispatch(fetchUser(id)));
+    lodash.chain(getState().posts).map('userId').uniq().forEach(id => dispatch(fetchUser(id))).value();
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = lodash.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
