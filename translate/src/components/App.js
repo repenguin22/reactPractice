2a1b61e3ac5a5af648a4a39a1714145113c1f70f
repenguin <devSomaPro@gmail.com>
2019-09-15 07:18:53 +0900

    basic React Context API

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from '../components/LanguageSelector';
 
 class App extends React.Component {
+
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    };
+
     render() {
-        return <div className="ui container">aaaa</div>;
+        return (
+            <div className="ui container">
+                <LanguageSelector onLanguageChange={this.onLanguageChange} />
+                <ColorContext.Provider value="red">
+                    <LanguageContext.Provider value={this.state.language}>
+                        <UserCreate />
+                    </LanguageContext.Provider>
+                </ColorContext.Provider>
+            </div>
+        );
     }
 }
 
