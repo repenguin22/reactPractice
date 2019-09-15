8875a5b80d71e4a4f72bfce882ad3e1e1cf275f7
repenguin <devSomaPro@gmail.com>
2019-09-15 10:19:18 +0900

    hook complete

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
 
-class App extends React.Component {
-    state = { resource: 'posts' };
-    render() {
-        return (
+const App = () => {
+    const [resource, setResource] = useState('posts');
+    return (
+        <div>
+            <UserList />
             <div>
-                <div>
-                    <button onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
-                    <button onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
-                </div>
-                {this.state.resource}
+                <button onClick={() => setResource('posts')}>Posts</button>
+                <button onClick={() => setResource('todos')}>Todos</button>
             </div>
-        );
-    }
+            <ResourceList resource={resource} />
+        </div>
+    );
 }
 
 export default App;
